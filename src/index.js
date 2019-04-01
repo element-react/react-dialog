95fcd85a8704edef53690559c61411ac2ebcb190
jxcao <jxcao@corp.netease.com>
2019-04-01 17:41:43 +0800

    feat: add alert confirm toas api

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ export { default } from './dialog';
 
 // 创建并返回dialog接口
 export const dialog = DialogManager.create();
+dialog.alert = (content, btn) => {
+  return dialog({
+    title: null,
+    content,
+    button: btn && btn.length ? btn : ['ok']
+  });
+};
 
-import React from 'react';
-import ReactDom from 'react-dom';
+dialog.toast = (content, timeout) => {
+  return dialog({
+    title: null,
+    content,
+    timeout: timeout || 2000,
+    button: null
+  });
+};
+
+dialog.confirm = (content, btn) => {
+  return dialog({
+    title: null,
+    content,
+    button: btn && btn.length ? btn : ['ok', 'cancel']
+  });
+};
