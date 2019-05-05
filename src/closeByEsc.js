7d91367673a71ae8db013dce2f4ca412ed59b830
jxcao <jxcao@corp.netease.com>
2019-05-05 15:06:22 +0800

    fix: fix length err

--- src/closeByEsc.js
+++ src/closeByEsc.js
@@ -4,7 +4,7 @@ import { ready } from './utils';
 
 ready(function () {
   document.body.addEventListener('keydown', function (event) {
-    if (event.keyCode === 27) {
+    if (event.keyCode === 27 && dialogInstanceCache && dialogInstanceCache.length) {
       // 取最后一个弹窗
       const instance = dialogInstanceCache[dialogInstanceCache.length - 1];
       const { props } = instance;
